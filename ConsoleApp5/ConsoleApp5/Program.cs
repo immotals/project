4ce8d567915f8c5b4d7a185ee8ef2ad7d188a5e6
immotals <39756917+immotals@users.noreply.github.com>
2022-06-22 19:18:49 +0900

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Hello
+{
+    public class Program
+    {
+        static void PRINTCHAR(int x,  int y)
+        {
+            char a = '*';
+            char b = ' ';
+            for (int i = 1; i < x; i++)
+            {
+                Console.Write(b);
+            }
+            for (int i = 0; i < y; i++)
+            {
+                Console.Write(a);
+            }
+            Console.WriteLine();
+
+        }
+        static void Main(string[] args)
+        {
+            PRINTCHAR(5, 1);
+            PRINTCHAR(4, 3);
+            PRINTCHAR(3, 5);
+            PRINTCHAR(2, 7);
+            PRINTCHAR(1, 9);
+        }
+    }
+}
