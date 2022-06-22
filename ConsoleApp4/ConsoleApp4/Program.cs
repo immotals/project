ab41a7b6e7687add1fb1f8804c3b0c682ff66ef9
immotals <39756917+immotals@users.noreply.github.com>
2022-06-22 19:17:39 +0900

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Hello
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 6;
+            char c = '*';
+            for (int i=1; i <= k; i++)
+            {
+                for(int j=i; j < k; j++) {
+                    Console.Write(" "); 
+                }
+                for (int n = 1; n <= 2*i-1; n++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
