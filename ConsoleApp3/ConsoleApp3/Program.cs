93e4b5d060670775d6f63d7f5b1d888d83bdaf5b
immotals <39756917+immotals@users.noreply.github.com>
2022-06-22 19:16:10 +0900

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void SumAvg(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            int avg = sum / arr.Length;
+            Console.WriteLine("합: " + sum);
+            Console.WriteLine("평균: " + avg);
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 25, 125, 78125, 625, 5 };
+            SumAvg(arr);
+        }
+    }
+}
