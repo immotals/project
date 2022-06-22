bd40ef5879d1bb1e92747a30de6488dd806a7fc3
immotals <39756917+immotals@users.noreply.github.com>
2022-06-22 19:12:38 +0900

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Hello
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 10)
+            {
+                i++;
+                if (i % 2 == 0)
+                {
+                    continue;
+                }
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
