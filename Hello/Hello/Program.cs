b408d377fd7d4013c4840ec640b0c00b1105881e
immotals <39756917+immotals@users.noreply.github.com>
2022-06-22 19:27:11 +0900

    Add files via upload

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;//Console단어를 생략(코드길이줄어둠)
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = int.Parse(ReadLine());
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+                WriteLine("윤년");
+            else
+                WriteLine("평년");
+            if (DateTime.IsLeapYear(year))
+                WriteLine("윤년");
+            else
+                WriteLine("평년");
+        }
+    }
+}
